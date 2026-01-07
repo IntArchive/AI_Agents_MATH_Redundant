b087909b98f65acda0b8801de443e9cd0cc388b9
MinhHai <ddinhminhhair@gmail.com>
2026-01-07 11:54:23 +0700

    Update adaptive dataloader for many file types

--- working/utils/dataloader.py
+++ working/utils/dataloader.py
@@ -16,6 +16,9 @@ def load_problem_column(file_path, target_problem_col):
         df = pd.read_csv(file_path)
     elif file_path.endswith('.xlsx') or file_path.endswith('.xls'):
         df = pd.read_excel(file_path)
+    elif file_path.endswith('.ods'):
+        from pandas_ods_reader import read_ods
+        df = read_ods(file_path)
     else:
         raise ValueError("Unsupported file type. Please provide a CSV or Excel file.")
 
@@ -23,3 +26,18 @@ def load_problem_column(file_path, target_problem_col):
         raise KeyError(f"Column '{target_problem_col}' not found in the file.")
 
     return df[target_problem_col]
+
+def load_data(file_path):
+    """
+    Load data from the given file with different file types and return the data.
+    """
+    if file_path.endswith('.ods'):
+        from pandas_ods_reader import read_ods
+        data = read_ods(file_path)
+    elif file_path.endswith('.xlsx') or file_path.endswith('.xls'):
+        data = pd.read_excel(file_path)
+    elif file_path.endswith('.csv'):
+        data = pd.read_csv(file_path)
+    else:
+        raise ValueError("Unsupported file type. Please provide a ODS, Excel, or CSV file.")
+    return data
